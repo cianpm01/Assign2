c6312ddb09221cd4dec61655ebc96cf3491fbe3b
cianpm01 <78463470+cianpm01@users.noreply.github.com>
2021-03-03 19:51:47 -0800

    Update FaceControl.java

--- app/src/main/java/com/example/cshw2/FaceControl.java
+++ app/src/main/java/com/example/cshw2/FaceControl.java
@@ -11,33 +11,47 @@ import androidx.annotation.RequiresApi;
 
 import java.util.Random;
 
+//@author Cian Murray
+//Date: 3/3/21
+
+
+//This class is the faceController class. It controlls changes made to the face. When the user interacts with the widgits when the application is running,
+//some elemnet of the face class needs to change, and those changes are done in this class.
+//It sets colors, hair styles and seekbar progress based on user input
 
 public class FaceControl implements View.OnClickListener, CompoundButton.OnCheckedChangeListener, SeekBar.OnSeekBarChangeListener, AdapterView.OnItemSelectedListener {
     private Face view;
     Random rand = new Random();
-
+ //Instance variables
+     
+    // Seekbars to be used in a setter method
     SeekBar seekOne;
     SeekBar seekTwo;
     SeekBar seekThree;
+    
+   
+     //ints representing red, green and blue to be used when adjusting the color
     int red;
     int green;
     int blue;
 
 
-
+    //Constructor   
     public FaceControl(Face fav)
     {
         view = fav;
 
     }
-
+    
+    
+    //onClick method
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button:
-                // do your code
+                 //If the random button is clicked, all elements of the face are randomized
                 view.hairStyle = rand.nextInt(3);
                 view.skinColor = rand.nextInt(1 + 167772) -167772;
                 view.eyeColor = rand.nextInt(1 + 167772) -167772;
@@ -52,6 +66,7 @@ public class FaceControl implements View.OnClickListener, CompoundButton.OnCheck
                 view.invalidate();
                 break;
 
+            //If the "hair" radio button is selected, the sliders reflect the hair color's rgb values
             case R.id.Hairbutton:
 
                  red = Color.red((view.hairColor));
@@ -77,6 +92,7 @@ public class FaceControl implements View.OnClickListener, CompoundButton.OnCheck
 
 
                 break;
+                //If the "skin" radio button is selected, the sliders reflect the skin color's rgb values
 
             case R.id.skinButton:
 
@@ -92,6 +108,7 @@ public class FaceControl implements View.OnClickListener, CompoundButton.OnCheck
                 view.invalidate();
 
                 break;
+                //If the "eye" radio button is selected, the sliders reflect the hair color's rgb values
 
             case R.id.eyeButton:
 
@@ -118,14 +135,14 @@ public class FaceControl implements View.OnClickListener, CompoundButton.OnCheck
     }
 
 }
-
+  //Method for nothing selected for the spinner. If no items are selected, the spinner will default to Style one
     public void onNothingSelected(AdapterView<?> parent) {
         view.hairStyle = 0;
         view.invalidate();
 
 
     }
-
+//This method sets the variable hairStyle in hte face class according to the item on the spinner the user selects
     public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
 
         switch(position)
@@ -169,6 +186,7 @@ public class FaceControl implements View.OnClickListener, CompoundButton.OnCheck
     public void onStopTrackingTouch(SeekBar seekBar) {
 
     }
+    //setter method
     public void setSeekBars(SeekBar r, SeekBar g, SeekBar y)
     {
         seekOne = r;
